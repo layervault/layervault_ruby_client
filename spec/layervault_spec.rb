9380b8738203f90b1013ca5f4296f037d3d83ce8
John McDowall <john@mcdowall.info>
2013-10-07 08:48:07 -0700

    Spec opts

---
+++ spec/layervault_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe "The LayerVault API client" do
+  
+  context "Creating the client" do
+    it "instantiates a client with proper configuration" do
+    end
+  end
+
+end
