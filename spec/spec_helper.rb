fedd51575e3ce681a918ae00ff072826ee8b1e8b
John McDowall <john@mcdowall.info>
2013-10-08 11:50:30 -0700

    VCR Config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,20 @@ require 'rspec'
 require 'json'
 require 'layervault'
 
+require 'vcr'
+VCR.configure do |c|
+  c.configure_rspec_metadata!
+
+  c.default_cassette_options = {
+    :serialize_with             => :json,
+    :preserve_exact_body_bytes  => true,
+    :decode_compressed_response => true
+  }
+  
+  c.cassette_library_dir = 'spec/cassettes'
+  c.hook_into :webmock
+end
+
 
 RSpec.configure do |config|
 
