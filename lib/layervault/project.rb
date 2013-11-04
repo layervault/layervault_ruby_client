edda83d9b2e2e0ca444779e67c5ac13a0b3793c4
Kelly Sutton <michael.k.sutton@gmail.com>
2013-11-03 23:11:52 -0500

    Forgot to set the instance in the project constructor

--- lib/layervault/project.rb
+++ lib/layervault/project.rb
@@ -4,7 +4,7 @@ module LayerVault
     class << self
       def for(organization, project)
         resp = MultiJson.decode(LayerVault.client.project(organization, project))
-        build_associations(resp, :folders, :files)
+        instance = build_associations(resp, :folders, :files)
         instance.set_context(organization: organization, project:project)
       end
     end
